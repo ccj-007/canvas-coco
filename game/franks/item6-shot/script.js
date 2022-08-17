6b976588b86ae921ec24fe180accc5112749b774
chen <596487930@qq.com>
2022-08-18 01:41:45 +0800

    shot

--- game/franks/item6-shot/script.js
+++ game/franks/item6-shot/script.js
@@ -5,17 +5,49 @@ canvas.height = window.innerHeight
 
 const collisionCanvas = document.getElementById('collisionCanvas')
 const collisionCtx = collisionCanvas.getContext('2d')
-collisionCtx.width = window.innerWidth
-collisionCtx.height = window.innerHeight
+collisionCanvas.width = window.innerWidth;
+collisionCanvas.height = window.innerHeight;
 
+console.log(window.innerWidth);
 let ravens = []
 ctx.font = '50px Impact'
 
 let score = 0
+let gameOver = false
 
 let timeToNextRaven = 0
 let ravenInterval = 500
 let lastTime = 0
+
+let trailings = []
+class Trailing {
+  constructor(x, y, size, color) {
+    this.color = color
+    this.size = size
+    this.x = x + this.size / 2 + Math.random() * 10 - 5
+    this.y = y + this.size / 3 + Math.random() * 10 - 5
+    this.radius = Math.random() * this.size / 10 + 2
+    this.maxRadius = Math.random() * 20 + 35
+    this.markedForDeletion = false
+    this.speedX = Math.random() * 1 + 0.5
+    this.color = color
+  }
+  //执行动作
+  update () {
+    this.x += this.speedX
+    this.radius += 0.8
+    if (this.radius > this.maxRadius - 5) this.markedForDeletion = true
+  }
+  draw () {
+    ctx.save()
+    ctx.globalAlpha = 1 - this.radius / this.maxRadius
+    ctx.beginPath();
+    ctx.fillStyle = this.color
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2)
+    ctx.fill()
+    ctx.restore()
+  }
+}
 
 class Raven {
   constructor() {
@@ -23,7 +55,7 @@ class Raven {
     this.spriteWidth = 271
     this.spriteHeight = 194
 
-    this.sizeModifier = Math.random() * .6 - .4
+    this.sizeModifier = Math.random() * .6 + .4
     this.width = this.spriteWidth * this.sizeModifier
     this.height = this.spriteHeight * this.sizeModifier
     this.x = canvas.width
@@ -41,6 +73,8 @@ class Raven {
     this.flapInterval = Math.random() * 50 + 50
     this.randomColors = [Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255)]
     this.color = 'rgb(' + this.randomColors[0] + ',' + this.randomColors[1] + ',' + this.randomColors[2] + ')'
+
+    this.hasTrail = Math.random() > .2
   }
   update (deltatime) {
     if (this.y < 0 || this.y > canvas.height - this.height) {
@@ -56,7 +90,16 @@ class Raven {
       if (this.frame > this.maxFrame) this.frame = 0
       else this.frame++
       this.timeSinceFlap = 0
+
+      //放入拖尾效果
+      if (this.hasTrail) {
+        for (let i = 0; i < 3; i++) {
+          trailings.push(new Trailing(this.x, this.y, this.width, this.color))
+        }
+      }
     }
+
+    if (this.x < 0 - this.width) gameOver = true
   }
   draw () {
     collisionCtx.fillStyle = this.color
@@ -66,22 +109,75 @@ class Raven {
   }
 }
 
+//爆炸动画
+let explosions = []
+class Explosion {
+  constructor(x, y, size) {
+    this.image = new Image()
+    this.image.src = '../static/collision/cloud.png'
+    this.x = x
+    this.y = y
+    this.size = size
+    this.spriteWidth = 200
+    this.spriteHeight = 179
+    this.frame = 0
+    this.sound = new Audio
+    this.sound.src = '../static/chew.wav'
+    this.timeSinceLastFrame = 0
+    this.frameInterval = 200
+    this.markedForDeletion = false
+  }
+  update (deltatime) {
+    if (this.frame === 0) this.sound.play()
+    this.timeSinceLastFrame += deltatime
+    if (this.timeSinceLastFrame > this.frameInterval) {
+      this.frame++
+
+      if (this.frame > 5) {
+        this.markedForDeletion = false
+      }
+    }
+  }
+  draw () {
+    ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteWidth, this.x, this.y, this.size, this.size)
+  }
+}
+
+
+
 function drawScore () {
   ctx.fillStyle = 'black'
   ctx.fillText('Score: ' + score, 50, 75)
   ctx.fillStyle = 'white'
   ctx.fillText('Score: ' + score, 55, 80)
 }
+function drawGameOver () {
+  ctx.textAlign = 'center'
+  ctx.fillStyle = 'black'
+  ctx.fillText('GAME OVER, your score is ' + score, canvas.width / 2, canvas.height / 2)
+  ctx.fillStyle = 'white'
+  ctx.fillText('GAME OVER, your score is ' + score, canvas.width / 2 + 5, canvas.height / 2 + 5)
+}
 
 window.addEventListener('click', function (e) {
   const pixelColor = collisionCtx.getImageData(e.x, e.y, 1, 1)
-  console.log(pixelColor);
+
+  let pc = pixelColor.data
+
+  console.log(pc);
+  ravens.forEach(object => {
+    if (object.randomColors[0] === pc[0] && object.randomColors[1] === pc[1] && object.randomColors[2] === pc[2]) {
+      object.markedForDeletion = true
+      score++
+      explosions.push(new Explosion(object.x, object.y, object.width))
+    }
+  })
 })
 
-//通过帧的控制
+//通过帧的控制  
 function animate (timestamp) {
-  collisionCtx.clearRect(0, 0, collisionCtx.width, collisionCtx.height)
   ctx.clearRect(0, 0, canvas.width, canvas.height)
+  collisionCtx.clearRect(0, 0, canvas.width, canvas.height)
 
   let deltatime = timestamp - lastTime
   lastTime = timestamp
@@ -96,11 +192,17 @@ function animate (timestamp) {
   }
   drawScore();
   //这里的扩展运算符可以更直观序列化的展示所有要绘制的内容
-  [...ravens].forEach(object => object.update(deltatime));
-  [...ravens].forEach(object => object.draw());
+  [...trailings, ...ravens, ...explosions].forEach(object => object.update(deltatime));
+  [...trailings, ...ravens, ...explosions].forEach(object => object.draw());
 
   ravens = ravens.filter(object => !object.markedForDeletion)
-  requestAnimationFrame(animate)
+
+  explosions = explosions.filter(object => !object.markedForDeletion)
+  trailings = trailings.filter(object => !object.markedForDeletion)
+
+  //游戏结束就取消
+  if (!gameOver) requestAnimationFrame(animate)
+  else drawGameOver()
 }
 
 animate(0)
